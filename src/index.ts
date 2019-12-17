72bfeb6c951431e259e9eaa490fa78e049ffde2c
DayDun <TheDayDun@gmail.com>
2019-12-17 23:33:31 +0100

    Moved try catch into vm to hopefully make it slightly more safe

--- src/index.ts
+++ src/index.ts
@@ -30,51 +30,56 @@ const handleMessage = (message: Message) => {
     return;
   }
 
-  const recursiveCatch = (cb) => {
-    try {
-      cb();
-    } catch (err) {
-      recursiveCatch(() => {
-        const msg = String(err) || 'error message cannot be empty';
-        message.channel.send(msg);
-        console.log('Err:', msg);
-      });
-    }
-  };
-
-  recursiveCatch(() => {
-    const out = [];
-    const vm = new VM({
-      eval: false,
-      wasm: false,
-      timeout: 100,
-      sandbox: {
-        console: {
-          log: (...args: any[]) => out.push(args.map(String).join('')),
-          info: (...args: any[]) => out.push(args.map(String).join('')),
-          warn: (...args: any[]) => out.push(args.map(String).join('')),
-          error: (...args: any[]) => out.push(args.map(String).join('')),
-          debug: (...args: any[]) => out.push(args.map(String).join('')),
-        },
-        fs: {
-          readSync: mfs.readSync.bind(mfs),
-          readFileSync: mfs.readFileSync.bind(mfs),
-          readdirSync: mfs.readdirSync.bind(mfs),
-          statSync: mfs.statSync.bind(mfs),
-          fstatSync: mfs.fstatSync.bind(mfs),
-          lstatSync: mfs.lstatSync.bind(mfs),
-        },
+  const out = [];
+  const vm = new VM({
+    eval: false,
+    wasm: false,
+    timeout: 100,
+    sandbox: {
+      console: {
+        log: (...args: any[]) => out.push(args.map(String).join('')),
+        info: (...args: any[]) => out.push(args.map(String).join('')),
+        warn: (...args: any[]) => out.push(args.map(String).join('')),
+        error: (...args: any[]) => out.push(args.map(String).join('')),
+        debug: (...args: any[]) => out.push(args.map(String).join('')),
       },
-    });
-    console.log('In:', message.content);
-    const returnValue = vm.run(message.content);
-    out.push(returnValue);
-    const outStr = out.join('\n');
-    if (outStr.length > 0) {
-      message.channel.send(outStr);
-      console.log('Out:', outStr);
-    }
+      fs: {
+        readSync: mfs.readSync.bind(mfs),
+        readFileSync: mfs.readFileSync.bind(mfs),
+        readdirSync: mfs.readdirSync.bind(mfs),
+        statSync: mfs.statSync.bind(mfs),
+        fstatSync: mfs.fstatSync.bind(mfs),
+        lstatSync: mfs.lstatSync.bind(mfs),
+      },
+    },
   });
+  console.log('In:', message.content);
+  let returnValue;
+  try {
+    returnValue = vm.run(`
+      try {
+        String(eval(\`${message.content.replace(/\\/g, "\\\\").replace(/`/g, "\\`")}\`));
+      } catch(err) {
+        try {
+          String(err);
+        } catch(err) {
+          "Error while erroring";
+        }
+      }
+    `);
+  } catch(e) {
+    returnValue = "Error while running sandbox";
+  }
+  if (typeof returnValue !== "string") {
+    returnValue = "Return value has to be stringifyable";
+  }
+  
+  out.push(returnValue);
+  const outStr = out.join('\n');
+  if (outStr.length > 0) {
+    message.channel.send(outStr);
+    console.log('Out:', outStr);
+  }
 };
 
 client.on('ready', () => {
